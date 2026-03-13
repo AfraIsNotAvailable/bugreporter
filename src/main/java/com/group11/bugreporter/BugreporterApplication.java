adc3bd2952ab562ce5c0870350fdd8b1621beb1e
Serban Afrasinei <afrasinei.serban@gmail.com>
2026-03-13 13:19:44 +0200

    Initial commit after project initialization

---
+++ src/main/java/com/group11/bugreporter/BugreporterApplication.java
@@ -0,0 +1,13 @@
+package com.group11.bugreporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugreporterApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BugreporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group11/bugreporter/BugreporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group11.bugreporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugreporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
